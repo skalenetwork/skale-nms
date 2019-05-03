58315fc1ade497cbb364655289a710c93126178e
Alex Sheverdin <alex@skalelabs.com>
2019-05-03 20:09:55 +0300

    SKALE-826 Separate sla unit tests from integration ones

---
+++ tests/test_integr_sla.py
@@ -0,0 +1,55 @@
+#   -*- coding: utf-8 -*-
+#
+#   This file is part of SLA
+#
+#   Copyright (C) 2019 SKALE Labs
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from sla import sla_agent as sla
+from tests import preparation
+from tools.helper import init_skale
+
+
+def setup_module(module):
+    global nodes_count_before, nodes_count_to_add
+    ids = preparation.get_active_ids()
+    print(f'ids = {ids}')
+    nodes_count_before = len(ids)
+    max_id = max(ids) if len(ids) else -1
+    print(f'max_id = {max_id}')
+    print(f'nodes count before = {nodes_count_before}')
+    nodes_count_to_add = 2
+    preparation.create_set_of_nodes(max_id + 1, nodes_count_to_add)
+
+
+def test_nodes_are_created():
+
+    nodes_count_after = len(preparation.get_active_ids())
+    print(f'wait nodes_number = {nodes_count_before + nodes_count_to_add}')
+    print(f'got nodes_number = {nodes_count_after}')
+
+    assert nodes_count_after == nodes_count_before + nodes_count_to_add
+
+
+def test_get_validated_nodes():
+    skale = init_skale()
+    cur_nodes_count = nodes_count_before + nodes_count_to_add
+    cur_node_id = cur_nodes_count - 2
+    print(f'cur_node = {cur_node_id}')
+    validator = sla.Validator(skale, cur_node_id)
+    nodes = validator.get_validated_nodes()
+    print(f'nodes = {nodes}')
+    assert type(nodes) is list
+    assert any(node.get('id') == cur_nodes_count - 1 for node in nodes)
--- tests/test_sla.py
+++ tests/test_sla.py
@@ -17,31 +17,21 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import os
+
 from sla import ping, sim
 from sla import sla_agent as sla
-from tests import preparation
-from tools.helper import init_skale
+from tools.config import LOCAL_WALLET_FILENAME
+from tools.config_storage import ConfigStorage
+from tools.helper import TEST_DATA_DIR_PATH
+from tools.skale_mock import init_skale
+
+TEST_LOCAL_WALLET_PATH = os.path.join(TEST_DATA_DIR_PATH, LOCAL_WALLET_FILENAME)
+ID = 0
 
 
 def setup_module(module):
-    global nodes_count_before, nodes_count_to_add
-    ids = preparation.get_active_ids()
-    print(f'ids = {ids}')
-    nodes_count_before = len(ids)
-    max_id = max(ids) if len(ids) else -1
-    print(f'max_id = {max_id}')
-    print(f'nodes count before = {nodes_count_before}')
-    nodes_count_to_add = 2
-    preparation.create_set_of_nodes(max_id + 1, nodes_count_to_add)
-
-
-def test_nodes_are_created():
-
-    nodes_count_after = len(preparation.get_active_ids())
-    print(f'wait nodes_number = {nodes_count_before + nodes_count_to_add}')
-    print(f'got nodes_number = {nodes_count_after}')
-
-    assert nodes_count_after == nodes_count_before + nodes_count_to_add
+    prepare_wallets(2)
 
 
 def test_get_node_metrics_pos():
@@ -81,11 +71,17 @@ def test_generate_node_metrics():
 
 def test_get_validated_nodes():
     skale = init_skale()
-    cur_nodes_count = nodes_count_before + nodes_count_to_add
-    cur_node_id = cur_nodes_count - 2
-    print(f'cur_node = {cur_node_id}')
-    validator = sla.Validator(skale, cur_node_id)
+    validator = sla.Validator(skale, ID)
     nodes = validator.get_validated_nodes()
-    print(f'nodes = {nodes}')
     assert type(nodes) is list
-    assert any(node.get('id') == cur_nodes_count - 1 for node in nodes)
+    reported_nodes = validator.validate_and_get_reported_nodes(nodes)
+    assert type(reported_nodes) is list
+    # print(f'Reported nodes = {reported_nodes}')
+
+
+def prepare_wallets(count):
+    account_dict = {"address": "0x0",
+                    "private_key": "0x0"}
+    for i in range(count):
+        local_wallet_config = ConfigStorage(TEST_LOCAL_WALLET_PATH + str(i))
+        local_wallet_config.update(account_dict)
