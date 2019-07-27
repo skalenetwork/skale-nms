f916beac1ed99b2c1dbc61b7a115a89b05f45c6a
Alex Sheverdin <alex@skalelabs.com>
2019-07-27 19:13:04 +0300

    SKALE-1176, SKALE-1188 test to run local geth and deploy SKALE manager

---
+++ test_data/deploy_sc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh &&
+sudo apt-get update &&
+sudo bash nodesource_setup.sh &&
+sudo apt-get install -y nodejs &&
+sudo apt-get install -y build-essential &&
+git clone -b feature/alpine-team https://$GITHUB_TOKEN\@github.com/skalenetwork/skale-manager.git &&
+cd skale-manager &&
+npm install &&
+PRIVATE_KEY=$ETH_PRIVATE_KEY ENDPOINT="http://127.0.0.1:1919"  ./node_modules/.bin/truffle migrate --network unique
+
