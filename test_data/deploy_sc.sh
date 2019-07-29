b7f2b5225a50769a95830aa44ede59b572612af9
Alex Sheverdin <alex@skalelabs.com>
2019-07-29 11:54:10 +0300

    SKALE-1176, SKALE-1188 Try install node js via nvm

--- test_data/deploy_sc.sh
+++ test_data/deploy_sc.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh &&
+#curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh &&
 sudo apt-get update &&
-sudo bash nodesource_setup.sh &&
-sudo apt-get install -y nodejs &&
+#sudo bash nodesource_setup.sh &&
+#sudo apt-get install -y nodejs &&
 sudo apt-get install -y build-essential &&
 git clone -b feature/alpine-team https://$GITHUB_TOKEN\@github.com/skalenetwork/skale-manager.git &&
 cd skale-manager &&
