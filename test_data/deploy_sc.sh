3bf598a68308129456c3c66991b68be2f054b300
Alex Sheverdin <alex@skalelabs.com>
2019-10-17 21:47:19 +0300

    SKALE-1662 Switch SKALE Manager branch to develop

--- test_data/deploy_sc.sh
+++ test_data/deploy_sc.sh
@@ -7,7 +7,7 @@ sudo apt-get update && # comment this line if use clear Ubuntu (not Travis)
 #sudo bash nodesource_setup.sh &&
 #sudo apt-get install -y nodejs &&
 sudo apt-get install -y build-essential &&
-git clone -b enhancement/SKALE-1684-Move-to-byzantium https://$GITHUB_TOKEN\@github.com/skalenetwork/skale-manager.git &&
+git clone -b develop https://$GITHUB_TOKEN\@github.com/skalenetwork/skale-manager.git &&
 cd skale-manager &&
 npm install &&
 PRIVATE_KEY=$ETH_PRIVATE_KEY ENDPOINT=$ENDPOINT  ./node_modules/.bin/truffle migrate --network unique
