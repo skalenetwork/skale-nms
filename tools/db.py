e0c7ac9ab8b7ab2042c5fbaf4b40e6f60f164379
Alex Sheverdin <alex@skalelabs.com>
2019-10-18 18:31:08 +0300

    SKALE-1662 Move db constants to config

--- tools/db.py
+++ tools/db.py
@@ -18,23 +18,17 @@
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-import os
-
 from peewee import BooleanField, CharField, CompositeKey, DateTimeField, IntegerField, Model, \
     MySQLDatabase, fn
 
+from tools.configs.db import DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER
 
-user = os.environ.get("DB_USER")
-password = os.environ.get("DB_PASSWORD")
-port = int(os.environ.get("DB_PORT"))
-db_name = 'db_skale'
-host = '127.0.0.1'
 
 dbhandle = MySQLDatabase(
-    db_name, user=user,
-    password=password,
-    host=host,
-    port=port
+    DB_NAME, user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT
 )
 
 
