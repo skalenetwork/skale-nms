94f452089d2acb313083d5d9f9fe6327246c1536
Alex Sheverdin <alex@skalelabs.com>
2019-10-28 20:26:18 +0200

    SKALE-1717 Publish all images in one script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-#NAME=metrics-viewer
-NAME=$1
+NAMES=("bounty-agent" "sla-manager")
+
+for i in "${NAMES[@]}"
+do
+  echo "$i"
+
+NAME=$i
+
 REPO_NAME=skalelabshub/$NAME
 IMAGE_NAME=$REPO_NAME:$VERSION
 LATEST_IMAGE_NAME=$REPO_NAME:latest
@@ -18,3 +24,6 @@ if [ "$RELEASE" = true ]
 then
     docker push $LATEST_IMAGE_NAME || exit $?
 fi
+
+done
+
