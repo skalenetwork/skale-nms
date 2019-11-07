45f5f94d07696666bca2b11e454bc24d86079024
Alex Sheverdin <alex@skalelabs.com>
2019-11-07 20:24:21 +0200

    SKALE-1747 Replace VERSION by TAG

--- bounty/build.sh
+++ bounty/build.sh
@@ -3,26 +3,26 @@
 # Use for manual building/publishing of bounty-agent docker image
 # Use "p" parameter to publish after build
 
-VERSION=$(cat VERSION)-test
+VERSION=$(cat VERSION)
+TAG=$VERSION-test
 LINE="----------------------------------------"
 
 echo "Building SKALE Bounty agent v${VERSION}..."
-docker build -t skalelabshub/bounty-agent:${VERSION} . -f ./Dockerfile.bounty-agent &&
+docker build -t skalelabshub/bounty-agent:${TAG} . -f ./Dockerfile.bounty-agent &&
 echo "Bounty agent v${VERSION} was built successfully" &&
 if [ "$1" == "p" ]
 then
     echo "$LINE"
     echo "Prepare to publish to Docker Hub..."
-    docker tag skalelabshub/bounty-agent:${VERSION} skalelabshub/bounty-agent:latest &&
+    docker tag skalelabshub/bounty-agent:${TAG} skalelabshub/bounty-agent:latest &&
     echo "Bounty agent v${VERSION} was tagged to the latest successfully" &&
     echo "$LINE"
     echo "Pushing Bounty agent v${VERSION}" &&
-    docker push skalelabshub/bounty-agent:${VERSION} &&
+    docker push skalelabshub/bounty-agent:${TAG} &&
     echo "$LINE"
     echo "Bounty agent v${VERSION} was pushed successfully" &&
-    echo "-----------------------------" &&
     echo "$LINE"
     docker push skalelabshub/bounty-agent:latest &&
     echo "$LINE"
-    echo "Bounty agent latest was pushed successfully"
+    echo "Bounty agent tagged latest was pushed successfully"
 fi
--- sla/build.sh
+++ sla/build.sh
@@ -3,25 +3,27 @@
 # Use for manual building/publishing of bounty-agent docker image
 # Use "p" parameter to publish after build
 
-VERSION=$(cat VERSION)-test
+VERSION=$(cat VERSION)
+TAG=$VERSION-test
+
 LINE="----------------------------------------"
 
 echo "Building SKALE SLA agent v${VERSION}..."
-docker build -t skalelabshub/sla-agent:${VERSION} . -f ./Dockerfile.sla-agent &&
+docker build -t skalelabshub/sla-agent:${TAG} . -f ./Dockerfile.sla-agent &&
 echo "SLA agent v${VERSION} was built successfully" &&
 if [ "$1" == "p" ]
 then
     echo "$LINE"
     echo "Prepare to publish to Docker Hub..."
-    docker tag skalelabshub/sla-agent:${VERSION} skalelabshub/sla-agent:latest &&
+    docker tag skalelabshub/sla-agent:${TAG} skalelabshub/sla-agent:latest &&
     echo "SLA agent v${VERSION} was tagged to the latest successfully" &&
     echo "$LINE"
     echo "Pushing SLA agent v${VERSION}" &&
-    docker push skalelabshub/sla-agent:${VERSION} &&
+    docker push skalelabshub/sla-agent:${TAG} &&
     echo "$LINE"
     echo "SLA agent v${VERSION} was pushed successfully" &&
     echo "$LINE"
     docker push skalelabshub/sla-agent:latest &&
     echo "$LINE"
-    echo "SLA agent latest was pushed successfully"
+    echo "SLA agent tagged latest was pushed successfully"
 fi
