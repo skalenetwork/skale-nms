6e1ffa9021ac4c67e52d0c0dc109d5e8b3d9c884
Alex Sheverdin <alex@skalelabs.com>
2019-12-10 21:24:00 +0200

    SKALE-1751 Add more get_containers_healthcheck tests

--- tests/unit/test_healthcheck.py
+++ tests/unit/test_healthcheck.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest import mock
 from tools.helper import get_containers_healthcheck  # MyGreatClass
+import requests
 
 
 # This method will be used by the mock to replace requests.get
@@ -19,10 +20,20 @@ def mocked_requests_get(*args, **kwargs):
         return MockResponse({'res': 1, 'data': data1}, 200)
     if args[0] == 'http://url_bad1:3007/healthchecks/containers':
         return MockResponse({'res': 1, 'data': data2}, 200)
-    elif args[0] == 'http://url_bad2:3007/healthchecks/containers':
+    if args[0] == 'http://url_bad2:3007/healthchecks/containers':
         return MockResponse({'res': 0, 'data': data1, 'errors': ["Error1"]}, 200)
+    elif args[0] == 'http://url_bad3:3007/healthchecks/containers':
+        return MockResponse({'res': 1, 'data': data1}, 500)
 
     return MockResponse(None, 404)
+
+
+def connection_error(*args, **kwargs):
+    raise requests.exceptions.ConnectionError
+
+
+def unknown_error(*args, **kwargs):
+    raise Exception
 
 
 @mock.patch('tools.helper.requests.get', side_effect=mocked_requests_get)
@@ -32,6 +43,22 @@ def test_healthcheck(mock_get):
     res = get_containers_healthcheck('url_bad1', False)
     assert res == 1
     res = get_containers_healthcheck('url_bad2', False)
+    assert res == 1
+    res = get_containers_healthcheck('url_bad3', False)
+    assert res == 1
+    res = get_containers_healthcheck('url_bad4', False)
+    assert res == 1
+
+
+@mock.patch('tools.helper.requests.get', side_effect=connection_error)
+def test_healthcheck_connection_error(mock_get):
+    res = get_containers_healthcheck('url_ok', False)
+    assert res == 1
+
+
+@mock.patch('tools.helper.requests.get', side_effect=unknown_error)
+def test_healthcheck_unknown_error(mock_get):
+    res = get_containers_healthcheck('url_ok', False)
     assert res == 1
 
 
