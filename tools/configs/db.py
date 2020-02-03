949da46584354fa629ed7ea58802e9c4781197be
Alex Sheverdin <alex@skalelabs.com>
2020-02-03 18:11:48 +0200

    SKALE-2073 Change DB_HOST to localhost

--- tools/configs/db.py
+++ tools/configs/db.py
@@ -1,11 +1,7 @@
 import os
-from tools.configs import ENV
 
 DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_PORT = int(os.environ.get("DB_PORT"))
 DB_NAME = 'db_skale'
-if ENV == 'DEV':
-    DB_HOST = '127.0.0.1'
-else:
-    DB_HOST = 'mysql'
+DB_HOST = '127.0.0.1'
